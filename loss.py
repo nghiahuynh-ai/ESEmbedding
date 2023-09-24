1237134cc3626acbbcc875bae28977f302f25d65
nghiahuynh-ai <huynhnguyenhieunghia1999@gmail.com>
2023-09-24 09:47:11 +0700

    update

--- loss.py
+++ loss.py
@@ -10,9 +10,17 @@ class ContrastiveLoss(nn.Module):
 
     def forward(self, x0, x1, y):
         # euclidian distance
+        print('x0: ', x0.shape)
+        print('--------------------------------')
+        print('X1: ', x1.shape)
+        
         diff = x0 - x1
         dist_sq = torch.sum(torch.pow(diff, 2), 1)
         dist = torch.sqrt(dist_sq)
+        
+        print('dist: ', dist.shape)
+        print('--------------------------------')
+        print('y: ', y.shape)
 
         mdist = self.margin - dist
         dist = torch.clamp(mdist, min=0.0)
